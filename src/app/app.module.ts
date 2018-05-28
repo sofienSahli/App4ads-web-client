bdf947ae72fe50b5959cd8e72b6f09ab7aa78216
sofien <sofien.sahli@esprit.tn>
2018-05-28 02:37:38 +0100

    second push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { AppComponent } from './pages/app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import {FormsModule} from '@angular/forms';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CardListComponent } from './card-list/card-list.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    NavBarComponent,
+    CardListComponent
   ],
   imports: [
-    BrowserModule
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
