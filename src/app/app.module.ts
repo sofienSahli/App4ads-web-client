bf0c57cb825dfbe8473d8767760f5f06ab326594
sofien <sofien.sahli@esprit.tn>
2018-05-31 17:05:05 +0100

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,40 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './pages/app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import {FormsModule} from '@angular/forms';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CardListComponent } from './card-list/card-list.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { NavDrawerMenuComponent } from './nav-drawer-menu/nav-drawer-menu.component';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    NavBarComponent,
-    CardListComponent
+    CardListComponent,
+    NavDrawerMenuComponent,
+    
   ],
   imports: [
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatSidenavModule,
     MatIconModule,
     MatInputModule,
     MatMenuModule,
     MatToolbarModule,
     BrowserAnimationsModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
